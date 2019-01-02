516e92a17203d7e1f3fccd119ac54e03771c36fc
epita1 <epita1@etf.unsa.ba>
2019-01-02 16:45:41 +0100

    Pocetni commit.

--- test/main/java/ba/unsa/etf/rpr/GeografijaDAOTest.java
+++ test/main/java/ba/unsa/etf/rpr/GeografijaDAOTest.java
@@ -1,4 +1,4 @@
-package ba.unsa.etf.rpr;
+package main.java.ba.unsa.etf.rpr;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
--- test/main/java/ba/unsa/etf/rpr/MainTest.java
+++ test/main/java/ba/unsa/etf/rpr/MainTest.java
@@ -1,4 +1,4 @@
-package ba.unsa.etf.rpr;
+package main.java.ba.unsa.etf.rpr;
 
 import static org.junit.jupiter.api.Assertions.*;
 
