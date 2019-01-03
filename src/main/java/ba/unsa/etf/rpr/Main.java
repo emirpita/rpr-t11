3550187494fabe05b129861fc2b8ddc06d8bb477
epita1 <epita1@etf.unsa.ba>
2019-01-03 11:25:20 +0100

    Popravljeno krahiranje GUI-a

--- src/main/java/ba/unsa/etf/rpr/Main.java
+++ src/main/java/ba/unsa/etf/rpr/Main.java
@@ -35,7 +35,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("gui.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui.fxml"));
         DrzavaModel drzavaModel = new DrzavaModel(baza);
         GradModel gradModel = new GradModel(baza);
         loader.setController(new Controller(baza, drzavaModel, gradModel));
